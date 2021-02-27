a25bcb4a5b307d61ab5c487c2d21438240f3c46e
PryseOlg <oleg.kuts.02@mail.ru>
2021-02-27 21:43:02 +0500

    Refactoring code

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -5,84 +5,93 @@ namespace Chess
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            while (true)
-            {
-                Point[] MassivePoints;
-                do
-                {
+        static void Main(string[] args) {
+            Vector vector;
+            while (true) {
+                do {
                     Console.WriteLine("Введите две координаты");
-                    //string coordinate = Console.ReadLine();
-                    MassivePoints = GetMassivePoints();
+                    Console.Write($"Введите 1-ую координату:");
+                    string firstcoordinate = Console.ReadLine();
+                    Console.Write($"Введите 2-ую координату:");
+                    string secondcoordinate = Console.ReadLine();
+                    vector = new Vector(firstcoordinate, secondcoordinate);
                 }
-                while (MassivePoints == null);
+                while (!vector.Exist());
 
-                if (GetCheckStepKnight(MassivePoints[0], MassivePoints[1]))
-                {
-                    Console.WriteLine("You are breathtaking");
+                if (GetCheckStepKnight(vector)) {
+                    Console.WriteLine("Верно");
                     break;
                 }
-                else Console.WriteLine("You are not breathtaking");
+                else Console.WriteLine("Некорректный ход");
 
             }
         }
-        static Point GetCoords(string Coord)
+
+        static bool GetCheckStepKnight(Vector vector) {
+            return (vector.From.X != vector.To.X) && (vector.From.Y != vector.To.Y)
+                   && (((Math.Abs(vector.From.X - vector.To.X)) + Math.Abs(vector.From.Y - vector.To.Y)) == 3);
+        }
+        
+    }
+
+    public class Vector {
+        private Point _from; // приватные поля
+        private Point _to; // приватные поля
+
+        public Point From {
+            get => _from;
+            set => _from = value;
+        } //пропертиес
+        public Point To
         {
-            if (Coord.Length != 2)
-                return Point.Empty;
+            get => _to;
+            set => _to = value;
+        } // пропертиес
+        
+        public Vector(Point from, Point to) {
+            this._from = from;
+            this._to = to;
+        } //конструктор
 
-            char FirstCoord = Coord[0];
-            char SecondCoord = Coord[1];
-            if (!IsLetterCorrect(FirstCoord) || !IsDigitCorrect(SecondCoord))
-                return Point.Empty;
-            int X = FirstCoord - (Char.IsUpper(FirstCoord) ? 64 : 96);
-            int Y = SecondCoord - '0';
-            return new Point(X, Y);
-
+        public Vector(string from, string to) {
+            this._from = GetCoords(from);
+            this._to = GetCoords(to);
         }
 
-        static bool IsLetterCorrect(char Letter)
+        public bool Exist()
+        {
+            return ((_from != Point.Empty) && (_to != Point.Empty));
+        }
+        private static bool IsLetterCorrect(char letter)
         {
             
-            if (!Char.IsLetter(Letter))
+            if (!Char.IsLetter(letter))
                 return false;
 
-            bool IsInLeftBorder = (Char.IsUpper(Letter) ? 'A' : 'a' ) <= Letter;
-            bool IsInRightBorder = Letter <= (char.IsUpper(Letter) ? 'H' : 'h');
+            bool isInLeftBorder = (Char.IsUpper(letter) ? 'A' : 'a' ) <= letter;
+            bool isInRightBorder = letter <= (char.IsUpper(letter) ? 'H' : 'h');
 
-            return (IsInLeftBorder && IsInRightBorder);
+            return (isInLeftBorder && isInRightBorder);
         }
-
-        static bool IsDigitCorrect(char Digit)
+        private static bool IsDigitCorrect(char digit)
         {            
-            if (!Char.IsDigit(Digit))
+            if (!Char.IsDigit(digit))
                 return false;
 
-            return (Digit >= '1' && Digit <= '8');         
+            return (digit >= '1' && digit <= '8');         
         }
-
-        static Point[] GetMassivePoints()
+        private static Point GetCoords(string coord)
         {
-            Point[] points = new Point[2];
-            for (int i = 0; i < points.Length; i++)
-            {
-                Console.Write($"Введите {i+1}-ую координату: ");
-                points[i] = GetCoords(Console.ReadLine());
-                if (points[i] == Point.Empty)
-                    return null;             
-            }
-            return points;
-        } //
+            if (coord.Length != 2)
+                return Point.Empty;
 
-        static bool GetCheckStepKnight(Point PointFrom, Point PointTo)
-        {
-            return (PointFrom.X != PointTo.X) && (PointFrom.Y != PointTo.Y) 
-                && (((Math.Abs(PointFrom.X - PointTo.X)) + Math.Abs(PointFrom.Y - PointTo.Y)) == 3);
-
-               
-
-
+            char abscissa = coord[0];
+            char ordinate = coord[1];
+            if (!IsLetterCorrect(abscissa) || !IsDigitCorrect(ordinate))
+                return Point.Empty;
+            int x = abscissa - (Char.IsUpper(abscissa) ? 64 : 96);
+            int y = ordinate - '0';
+            return new Point(x, y);
         }
     }
 }
